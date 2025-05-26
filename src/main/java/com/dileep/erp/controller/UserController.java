e360cbed84c784f993277611b06cd931235ea694
dileepkumarnagar <dileepnagar016@gmail.com>
2025-05-26 13:37:49 +0530

    Added update User function

--- src/main/java/com/dileep/erp/controller/UserController.java
+++ src/main/java/com/dileep/erp/controller/UserController.java
@@ -16,18 +16,38 @@ public class UserController {
     private UserRepository userRepository;
 
     @PostMapping("/user")
-    private User newUser(@RequestBody User user){
+    private User newUser(@RequestBody User user) {
         return userRepository.save(user);
     }
 
     @GetMapping("/users")
-    private List<User> getAllUsers(){
+    private List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
     @GetMapping("/user/{id}")
-    private User getUserById(@PathVariable Long id){
-        return userRepository.findById(id)
-                .orElseThrow(()-> new UserNotFoundException(id));
+    private User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @PutMapping("user/{id}")
+    private User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userRepository.findById(id).map(user -> {
+            user.setUsername(newUser.getUsername());
+            user.setEmail(newUser.getEmail());
+            user.setName(newUser.getName());
+            user.setEmail(newUser.getEmail());
+            return userRepository.save(user);
+        }).orElseThrow(() -> new UserNotFoundException(id));
+
+    }
+
+    @DeleteMapping("/user/{id}")
+    private String deleteUser(@PathVariable Long id) {
+        if (!userRepository.existsById(id)) {
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return "User with id " + id + " has been deleted successfully .";
     }
 }
